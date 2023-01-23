fa3aafb172fd2460eea5bf06c1019633b737bc8b
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-01-23 17:19:33 -0300

    Fixing class names

--- src/components/Posts/Botton/Comments/Comment.jsx
+++ src/components/Posts/Botton/Comments/Comment.jsx
@@ -2,7 +2,7 @@ import { Like } from "../Actions/Like";
 
 export function Comment({ userName, comment }) {
   return (
-    <div className="comentario">
+    <div className="comentario-box">
       <div className="comentario">
         <strong>{userName}</strong> {comment}
       </div>
