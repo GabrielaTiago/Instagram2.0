08c44cf2fb0ebec668598392ba710580f33e07a6
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-01-23 09:40:14 -0300

    Creates the home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Footer, Header } from "../../components";
+import { Main } from "./Main";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
